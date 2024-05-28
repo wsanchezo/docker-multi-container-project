0b4ce1c68a792d190aa7dbb0870dae97c61e5b5c
axelRamses21 <75132941+jhojan-ramirez@users.noreply.github.com>
2024-05-27 20:52:35 -0500

    Commit Jhojan Alexander Ramirez Diaz 27/05/2024

--- docker-multi-container-project/app/app.py
+++ docker-multi-container-project/app/app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__) 
 
-@app.route('/') 
-def hello_world(): 
-    return 'Hello, World!' 
+@app.route('/')
+def hello_world(message="Hello, World!"):
+    commit_message = "I made my first commit :)"
+    return f"<h1>{message}</h1><p>{commit_message}</p>\n"
  
 if __name__ == '__main__': 
     app.run(host='0.0.0.0') 
