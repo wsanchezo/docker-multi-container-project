1ceb34c2241ce89cc1345909cbfce3561491e54c
IsaacDavidS <isaacdsolisb@gmail.com>
2024-05-28 00:47:27 -0500

    Demostración de comunicación entre contenedores

--- docker-multi-container-project/app/app.py
+++ docker-multi-container-project/app/app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world(message="Hello, World!"):
     commit_message = "I made my first commit :)"
-    return f"<h1>{message}</h1><p>{commit_message}</p>\n"
+    commit_message2 = "Demostración de cómo dos contenedores se comunican entre sí"
+    return f"<h1>{message}</h1><p>{commit_message}</p><p>{commit_message2}</p>\n"
  
 if __name__ == '__main__': 
     app.run(host='0.0.0.0') 
